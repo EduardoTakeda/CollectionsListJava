d6911f013d8d77f0288e64be0edb14437568c892
celinatakeda <takedacarina@gmail.com>
2021-07-01 17:17:16 -0300

    implementar CompareIde, Ordem por IDE ok

--- src/ExercicioLinguagemFavorita/ComparatorNomeAnoDeCriacaoIde.java
+++ src/ExercicioLinguagemFavorita/ComparatorNomeAnoDeCriacaoIde.java
@@ -13,15 +13,7 @@ public class ComparatorNomeAnoDeCriacaoIde implements Comparator<LinguagemFavori
             return o1.ide.compareToIgnoreCase(o2.ide);
     }
 
-//    @Override
-//        public int compare(LinguagemFavorita lf1, LinguagemFavorita lf2) {
-//            int nome = lf1.nome.compareToIgnoreCase(lf2.nome);
-//            int anoDeCriacao = Integer.compare(lf1.anoCriacao, lf2.anoCriacao);
-//            if (nome != 0) return nome;
-//            if (anoDeCriacao != 0) return anoDeCriacao;
-//            return lf1.ide.compareToIgnoreCase(lf2.ide);
-//        }
-//    }
+
 static class CompareIde implements Comparator<LinguagemFavorita>{
 
     @Override
@@ -29,14 +21,5 @@ static class CompareIde implements Comparator<LinguagemFavorita>{
         return o1.ide.compareToIgnoreCase(o2.ide);
     }
 }
-//
-//class ComparatorAnoDeCriacaoENome implements Comparator<LinguagemFavorita>{
-//
-//    @Override
-//    public int compare(LinguagemFavorita lf1, LinguagemFavorita lf2) {
-//        int anoDeCriacao = Integer.compare(lf1.anoDeCriacao, lf2.anoDeCriacao);
-//        if (anoDeCriacao != 0) return anoDeCriacao;
-//        return lf1.nome.compareToIgnoreCase(lf2.nome);
-//    }
 
 }
